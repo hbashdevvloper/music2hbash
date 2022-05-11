7af59bea69af1015b2a60f3e1a3e9a54a3ea8a91
Yy45r <104987611+Yy45r@users.noreply.github.com>
2022-05-11 17:36:34 +0300

    Update others.py

--- Yukki/Inline/others.py
+++ Yukki/Inline/others.py
@@ -11,7 +11,7 @@ def others_markup(videoid, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🔎 Search Lyrics",
+                text="🔎 كلمات البحث",
                 callback_data=f"lyrics {videoid}|{user_id}",
             )
         ],
@@ -27,17 +27,17 @@ def others_markup(videoid, user_id):
         ],
         [
             InlineKeyboardButton(
-                text="⬇️ Download Audio/Video",
+                text="‹ تحميل صوت & فيديو ›",
                 callback_data=f"audio_video_download {videoid}|{user_id}",
             )
         ],
         [
             InlineKeyboardButton(
-                text="⬅️ Go Back",
+                text="• رجوع •",
                 callback_data=f"pr_go_back_timer {videoid}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🗑 Close Menu",
+                text="• اغلاق القائمة •",
                 callback_data=f"close",
             ),
         ],
@@ -49,19 +49,19 @@ def download_markup(videoid, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⬇️ Get Audio",
+                text="• صۅت •",
                 callback_data=f"gets audio|{videoid}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⬇️ Get Video",
+                text="• فيديۅ •",
                 callback_data=f"gets video|{videoid}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="⬅️ Go Back", callback_data=f"goback {videoid}|{user_id}"
+                text="• رجوع •", callback_data=f"goback {videoid}|{user_id}"
             ),
-            InlineKeyboardButton(text="🗑 Close Menu", callback_data=f"close"),
+            InlineKeyboardButton(text="• اغلاق القائمة •", callback_data=f"close"),
         ],
     ]
     return buttons
